296cb4de419d2c1accac65213cd9b0ced25283aa
Sudesh Kumar Santhosh Kumar <santhosh@usc.edu>
2023-05-06 16:43:28 -0700

    Bug Fix Encoding.py

--- utils/Encoding.py
+++ utils/Encoding.py
@@ -11,10 +11,10 @@ import copy
 
 def StatisticalEncoding(dataFrame, categoricalFeatures, numericalFeatures): 
 
-    temp_df = copy.deepcopy(dataFrame)
+    temp_df = dataFrame[::]
 
-    X_train_temp = dataFrame.drop('class', axis=1)  # Select all the features except labels,
-    y_train_temp = dataFrame['class']  # Select only the 'class' column.
+    X_train_temp = temp_df.drop('class', axis=1)  # Select all the features except labels,
+    y_train_temp = temp_df['class']  # Select only the 'class' column.
     stats = ['mean', 'min', 'max', 'median']
 
     # Loop over all combinations of categorical and numerical features and calculate statistics
@@ -26,14 +26,18 @@ def StatisticalEncoding(dataFrame, categoricalFeatures, numericalFeatures):
                 # Create a new feature name based on the categorical feature and the statistic
                 new_feature_name = cat_feature + '_' + num_feature + '_' + stat
                 # Map the new feature to the data frame
-                dataFrame[new_feature_name] = dataFrame[cat_feature].map(group_stat)
+                # X_train_temp[new_feature_name] = X_train_temp[cat_feature].map(group_stat)
+
+                new_column = pd.DataFrame({new_feature_name: X_train_temp[cat_feature].map(group_stat)})
+
+                X_train_temp = pd.concat([X_train_temp, new_column], axis=1)
 
     # Drop the original categorical and numerical features
     X_train_temp.drop(categoricalFeatures, axis=1, inplace=True)
 
-    temp_df = pd.concat([X_train_temp, y_train_temp], axis=1)
+    temp_df_final = pd.concat([X_train_temp, y_train_temp], axis=1)
 
-    return temp_df
+    return temp_df_final
 
 
 def FrequencyEncoding():

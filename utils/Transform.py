f848ceeb2d5220ec14d8719b06bbf1dc4249f05d
Sudesh Kumar Santhosh Kumar <santhosh@usc.edu>
2023-05-07 18:32:18 -0700

    Updated iterative LDA

--- utils/Transform.py
+++ utils/Transform.py
@@ -7,8 +7,11 @@
 
 
 #Importing all necessary libraries
+import numpy as np
+import pandas as pd
 from sklearn.decomposition import PCA as sklearnPCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.feature_selection import SequentialFeatureSelector as SFS
 from sklearn.preprocessing import StandardScaler
 
 
@@ -25,7 +28,9 @@ def standardizeData(X):
     X_std = scaler.transform(X)
     print("Standardized the Train Data!")
 
-    return X_std
+    X_std_df = pd.DataFrame(X_std, columns=X.columns) 
+
+    return X_std_df
 
 
 # User-defined Function to perform PCA on the input Features.
@@ -37,9 +42,23 @@ def transformTrainData_PCA(X, components):
 
 
 # User-defined Function to perform LDA on the input Features.
-def transformTrainData_LDA(X, Y, components):
-    lda = LDA(n_components = 2)
+def transformTrainData_LDA(X, Y):
+    lda = LDA(n_components = 1)
     X_LDA = lda.fit_transform(X, Y)
 
     return X_LDA
+
+# User-defined Function to perform LDA on the input Features.
+def transformTrainData_IterLDA(X, Y, components):
+    lda = LDA()
+    # Define SFS algorithm
+    sfs = SFS(lda, n_features_to_select=components, direction='forward', cv=10)
+
+    # Fit SFS algorithm to data
+    sfs.fit(X, Y)
+
+    # Transform data to selected features
+    X_sfs = sfs.transform(X)
+
+    return X_sfs
 
